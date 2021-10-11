764c1ecff3ab9aeaccf7f167ba14632d6ceef8bb
Lenovo <2480785371@qq.com>
2021-10-11 18:02:55 +0800

    李迎澳到此一游

---
+++ src/test/java/com/user102/title/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user102.title.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
