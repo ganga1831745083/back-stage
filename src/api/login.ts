974fb1b0ae52b9ed6e37398d1a60f87c3f5828e0
awj <2727600681@qq.com>
2021-10-12 20:25:18 +0800

    login页面

---
+++ src/api/login.ts
@@ -0,0 +1,8 @@
+import request from '../utils/request';
+export const login = (name:string,password:string)=>{
+    return request({
+        url:'/admin/login',
+        method:'post',
+        data:{name:name,password:password}
+    })
+}
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,33 +1,72 @@
-import { Button, Checkbox, Form, Input, Space } from 'antd';
+import { Button, Checkbox, Form, Input, message, Space } from 'antd';
 import React, { useState, useEffect } from 'react';
+import { login } from '../../api/login';
 import  './login.less';
+import {set} from '../../utils/storage'
+
 const Login: React.FC<any> = (props) => {
-    const [from] = Form.useForm();
+    const [form] = Form.useForm();
+    //提交表单且数据验证成功后回调事件
+    const onFinish = (form:any)=>{
+        login(form.name,form.password).then(response =>{
+            const {code,msg,data} = response.data;
+            if(code===0){
+                //点击下次自动登录
+                if(form.remember){
+                    //设置七天后过期localStorage.setItem(key, val,7*24*60)
+                }else{
+                    
+                }
+                message.success(msg)
+            }else{
+                message.error(msg)
+            }
+        })
+    }
+    //提交表单且数据验证失败后回调事件	
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
         <div className='login-box'>
             <Form
+                className='form-box'
                 name='login'
-                form={from}
+                form={form}
                 labelCol={{ span: 8 }}
                 wrapperCol={{ span: 16 }}
                 initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
                 autoComplete="off"
             >
                 <Form.Item
                     label='用户名'
                     name='name'
-
+                    rules={[{ required: true, message: '用户名不能为空' }]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label='密码'
                     name='password'
+                    rules={[
+                        { 
+                            type: 'string', 
+                            //动态验证
+                            validator:(rule,value)=>{
+                                if(value.length>0 && value.length<6){
+                                    return Promise.reject('密码长度不能小于6位')
+                                }
+                                return Promise.resolve()
+                            }
+                        }
+                    ]}
                 >
                     <Input.Password />
                 </Form.Item>
                 <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                    <Checkbox>牢记密码</Checkbox>
+                    <Checkbox>下次自动登录</Checkbox>
                 </Form.Item>
 
                 <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
